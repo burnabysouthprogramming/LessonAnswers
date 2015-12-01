595847eecec85ff6cd726839a4ad0b260cc92c1a
burnabysouthprogramming <kallentu@gmail.com>
2015-11-30 21:54:13 -0800

    Create Solution - S. Yuan [NEED COMPLETION].c

---
+++ Lesson-3/Solution - S. Yuan [NEED COMPLETION].c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[11];
+    int age;
+    char gender[20];
+    char favoriteDrug[20];
+    long amountMoney;
+    
+    
+    printf("plz enter ur muddafukkin n4m3. \n");
+    scanf("%s", &name)
+    
+    printf("tell me how old u r. \n");
+    scanf("%d", &age)
+    
+    printf("wat is ur age. \n");
+    scanf("%s", &gender);
+    
+    printf("yo dawg whats ur fav drug m8. \n");
+    scanf("%s", &favoriteDrug);
+    
+    return 0;
+}
+
