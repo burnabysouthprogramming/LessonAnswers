d050d6f8df157bb1d643206c10900918a54d0f5c
Burnaby South Programming Club <kallentu@gmail.com>
2016-01-01 08:41:37 -0800

    Create Solution - D. Kovalenko.c

---
+++ Lesson-1/Solution - D. Kovalenko.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+int morreysCats = 12;
+int morreysCats = (morreysCats + 4);
+int totalCatFood = 1500;
+int remainingCatFood = (totalCatFood - morreysCats * 50);
+int foodPerCat = (remainingCatFood / morreysCats);
+
+	return 0;
+}
